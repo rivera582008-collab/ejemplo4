88b72e30bf3264baf15c30b8cb23ef72ea41573c
rivera582008-colaboración <rivera582008@gmail.com>
2026-03-12 19:06:59 -0600

    cambio de minusculas y mayusculas por segunda vez

---
+++ secion8_cadenasDeTexto.py
@@ -0,0 +1,78 @@
+# las comillas triples son las que se encargan de hacer
+# Cadenas de texto largas sin mdoficar el formato.
+# texto corto
+poema1 = " Es porque un pajarito de la montaña ha hecho su nido en el hueco de un arbol matinal"
+
+
+# textos largos ''' o """
+poema2 = """Yo conocí a una nena
+Tan bella
+Ella es como una doncella
+Ella, ella, tan bella
+Yo conocí a una nena
+Tan bella
+Ella es de lo más bonita
+Ella, ella, tan bella
+Cara linda
+Piel morena
+Canela, si tú la viera' como menea
+Tiene cuerpo de sirena
+Tan bella, tan bella
+Mi piel con ella
+Rozando en la arena
+Frente al mar
+Bajo luna llena
+Ella es la que mi corazón anhela
+Quiero tener esa nena """
+
+# print(poema1)
+
+# computadora <- indica que variable queres imprimir
+# con el metodo print() es una funcion y tiene tres estados
+# void - no devuelve nada
+# objeto -> debuelve un tipo de dato
+# mutabilidad -- siemprre debemos evitar transformar el objeto original
+# clases -> estereotipo (como un molde) las clases estan formadas por
+# propiedades (lloron, no se toman las cosas en serio)
+# Auto propiedades (color, tipo de motor (electrico o gasolina))
+# personas (ojos, color de pelo)
+# Acciones de un carro (frenar, acelerar, moverse, cargarse y descargarse)
+# string (es para cadenas de texto)
+# upper es la definicion de mayusculas
+# se va a llenar el contenido de poema
+
+
+# poema2_Mayusculas = poema2.upper()
+# print(poema2_Mayusculas)
+# convertir en minusculas
+# se llama al string. lower (es para minusculas)
+# poema2_minusculas = poema2.lower()
+# print(poema2_minusculas)
+
+mensaje = "hOlA KACe progRMando o qUe HaCe"
+# capitalize ayuda a que la primera letra de cada palabra sea mayuscula
+mensajeCorrecto = mensaje.capitalize()
+print(mensajeCorrecto)
+# las filipantes aventuras del gato con bolson y alfredo
+titulo = "las filipantes aventuras del gato con bolson y alfredo"
+tituloCorrecto = titulo.title()
+print(tituloCorrecto)
+
+# swapCase() permite canbiar entre mayusculas y minusculas
+swapCaseTitulo = tituloCorrecto.swapcase()
+print(swapCaseTitulo)
+
+nombre1 = "Edgar"
+nombre2 = "Alexander"
+comparar = nombre1 + nombre2
+print(comparar)
+
+# metodos de validacion para saber si un elemento tiene numero o letra
+# false tiene numeros o espacios
+# true tiene solo letras
+numero = "512"
+solo_letras = "El chico del apartamentos "
+Coro = "piribiribanban"
+
+quieroSoloLetras = numero.isalpha()
+print(quieroSoloLetras)
