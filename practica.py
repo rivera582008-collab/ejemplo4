c72d9c67c80b57112d599ba81b96aa8b3d1c1c3f
rivera582008-colaboración <rivera582008@gmail.com>
2026-03-19 16:46:10 -0600

    se agrega una practica de python para saber si estoy entendiendo la clase

---
+++ practica.py
@@ -0,0 +1,8 @@
+nombre = input("Ingrese su nombre")
+print("Hola" + nombre)
+
+numero = int(input("ingrese un numero para sumar" + nombre))
+numero2 = int(input("ingrese otro numero" + nombre))
+resultado = numero + numero2
+print("el resultado es:", resultado, nombre)
+print("gracias por probar esta calculadora" + nombre)
