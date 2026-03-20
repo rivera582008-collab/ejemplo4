2d4412733e2cd7789d3fc89ead70f01c9e7c7df7
rivera582008-colaboración <rivera582008@gmail.com>
2026-03-20 16:45:09 -0600

    se agrega los elementos de la semana 9

--- retroalimentacionSemana9.py
+++ retroalimentacionSemana9.py
@@ -1,3 +1,5 @@
+Serie = "fullmetal alchemits"
+
 ### cada variable tiene un espacio de memoria asignado
 
 ## cuando una vable cambia => se pierde la inmutabilidad
@@ -31,10 +33,6 @@
 
 
 # -------------------------------------------------------
-
-Serie = "fullmetal alchemits"
-
-
 def saludo(nombres):
     print(nombres)
 
@@ -46,13 +44,50 @@ def saludo(nombres):
 
 ## Colocar el nombre de la serie como titulo
 fmaTemu = Serie.title()
-saludo(Serie)
-saludo(fmaTemu)
-
+# saludo(Serie)
+# saludo(fmaTemu)
+fnaMayusculas = Serie.upper()
+saludo(fnaMayusculas)
 ## deprogracion Lineal
-fmaMayusculas = Serie.upper()
-saludo(fmaMayusculas)
+FullmetalCapitalizer = fnaMayusculas.swapcase().title()
+## cuando encadenamos funciones se indica que la salidad de la funcion actual
+# 3 es la entrada de la siguiente funcion.
 
-fmaMinusculas = Serie.lower()
-saludo(fmaMinusculas)
-# FullmetalCapitalizer = Serie.capitalize()
+saludo(FullmetalCapitalizer)
+
+## compara cadenas de texo
+comparar1 = "Ever"
+comparar2 = "Ever"
+
+# casefold para comparar y pasar a  minusculas
+variableTemporal = comparar2.casefold()
+comparar = comparar1.casefold() == comparar2.casefold()
+# print(comparar)
+## casefold nos dara true unicamente si los elementos son identicos sino nos indcara un false
+
+## para verificar si es nu nuemero o un caracter vamos a utilizar isalfa()
+clasicas2005 = "Gasolina"
+comprarisAlpha = clasicas2005.isalpha()
+# print(comprarisAlpha, 2005)
+# isalpha nos va a dar true si el string que se le esta enviando es unicamente letras
+# solo numeros isalnum
+letracancion = "lo que paso paso entre tu y yo"
+decada = "10"
+
+ejemplo = letracancion.isalnum()
+print(ejemplo)
+
+ejemplo = decada.isalnum()
+print(ejemplo)
+# isalnum es para compara solo numeros dara true cuandoh aya solo numeros
+comprobardecadas = decada.isdigit()
+print(comprobardecadas)
+
+
+nombre = input("Escriba su nombre completo")
+nombreMayusculas = nombre.upper()
+print(nombreMayusculas)
+nombreMinusculas = nombre.lower()
+print(nombreMinusculas)
+nombreprimeraletramayuscula = nombre.title()
+print(nombreprimeraletramayuscula)
