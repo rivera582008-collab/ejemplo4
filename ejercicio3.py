5d09a1b0274516466e999871306ea3c325a9d112
rivera582008-colaboración <rivera582008@gmail.com>
2026-03-20 16:50:07 -0600

    Ejercicio 3: Conteo de caracteres eliminando espacios con replace

---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+frase = input("Ingrese su frase favorita")
+quitarEspacios = frase.replace(" ", "")
+print(quitarEspacios)
+
+letrasSinEspacio = len(quitarEspacios)
+print("la cantidad de caracteres es", letrasSinEspacio)
