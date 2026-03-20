5f6a5ca5c9194a79ca7980bc6525fd938988647a
rivera582008-colaboración <rivera582008@gmail.com>
2026-03-20 16:48:17 -0600

    Ejercicio 1: Manipulación de mayúsculas, minúsculas y len()

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+nombre = input("Ingrese su nombre completo")
+nombreMayusculas = nombre.upper()
+print(nombreMayusculas)
+
+nombreMinusculas = nombre.lower()
+print(nombreMinusculas)
+
+nombreCaracteres = len(nombre)
+print("cantidad de caracteres", nombreCaracteres)
