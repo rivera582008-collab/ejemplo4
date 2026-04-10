1966fa6e4a96a72f7f176a5b34cebf9e99bf548e
rivera582008-colaboración <rivera582008@gmail.com>
2026-04-10 16:06:07 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 8: Uso de count y splitlines en bloques de texto multilínea

---
+++ Ejercicio8SegundoParcial.py
@@ -0,0 +1,8 @@
+poema = """Mata su luz un fuego abandonado. Sube su canto
+un pájaro enamorado. Tantas criaturas ávidas en mi
+silencio y esta pequeña lluvia que me acompaña."""
+print(poema)
+print("La letra ¨a¨ aparece:", poema.count("a"), "veces")
+
+poemaModificado = poema.splitlines()
+print(poemaModificado)
