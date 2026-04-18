42409cf70d764a1b2598495d995434d50c9ca45c
rivera582008-colaboración <rivera582008@gmail.com>
2026-04-18 15:31:57 -0600

    Conversor de número a día de la semana

---
+++ eje6.py
@@ -0,0 +1,18 @@
+dia = int(input("Ingrese un número entre 1 y 7 "))
+
+if dia == 1:
+    print(f"El día correspondiente para el número {dia} es LUNES")
+elif dia == 2:
+    print(f"El día correspondiente para el número {dia} es MARTES")
+elif dia == 3:
+    print(f"El día correspondiente para el número {dia} es MIERCOLES")
+elif dia == 4:
+    print(f"El día correspondiente para el número {dia} es JUEVES")
+elif dia == 5:
+    print(f"El día correspondiente para el número {dia} es VIERNES")
+elif dia == 6:
+    print(f"El día correspondiente para el número {dia} es SÁBADO")
+elif dia == 7:
+    print(f"El día correspondiente para el número {dia} es DOMINGO")
+else:
+    print("Ingrese un número valido")
