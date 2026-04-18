111962307d36405aed7122d34dffde2fe627c189
rivera582008-colaboración <rivera582008@gmail.com>
2026-04-18 15:30:41 -0600

    Verificador de números pares e impares

---
+++ eje4.py
@@ -0,0 +1,6 @@
+numero = int(input("Ingrese un numero para determinar si es par o impar"))
+
+if numero % 2 == 0:
+    print(f"El número {numero} es par")
+else:
+    print(f"El número {numero} es impar")
